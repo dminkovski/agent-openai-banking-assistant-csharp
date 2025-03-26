a7f72713c06b4fbd5bbe24c940cc3be89114bf4c
David Minkovski <dminkovski@microsoft.com>
2025-03-26 12:53:45 +0100

    Added account business api

---
+++ app/business-api/account-api/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿[ApiController]
+[Route("[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+    private readonly ILogger<AccountsController> _logger;
+
+    public AccountsController(IAccountService accountService, ILoggerFactory loggerFactory)
+    {
+        _accountService = accountService;
+        _logger = loggerFactory.CreateLogger<AccountsController>();
+    }
+
+    [HttpGet("{accountId}")]
+    public ActionResult<Account> GetAccountDetails(string accountId)
+    {
+        _logger.LogInformation("Received request to get account details for account id: {AccountId}", accountId);
+        return Ok(_accountService.GetAccountDetails(accountId));
+    }
+
+    [HttpGet("{accountId}/paymentmethods/{methodId}")]
+    public ActionResult<PaymentMethod> GetPaymentMethodDetails(string accountId, string methodId)
+    {
+        _logger.LogInformation("Received request to get payment method details for account id: {AccountId} and method id: {MethodId}",
+            accountId, methodId);
+        return Ok(_accountService.GetPaymentMethodDetails(methodId));
+    }
+
+    [HttpGet("{accountId}/registeredBeneficiaries")]
+    public ActionResult<List<Beneficiary>> GetBeneficiaryDetails(string accountId)
+    {
+        _logger.LogInformation("Received request to get beneficiary details for account id: {AccountId}", accountId);
+        return Ok(_accountService.GetRegisteredBeneficiary(accountId));
+    }
+}
---
+++ app/business-api/account-api/Models/ApiModels.cs
@@ -0,0 +1,34 @@
+﻿public record Account(
+    string id,
+    string userName,
+    string accountHolderFullName,
+    string currency,
+    string activationDate,
+    string balance,
+    List<PaymentMethodSummary> paymentMethods
+);
+public record PaymentMethodSummary(
+    string id,
+    string type,
+    string activationDate,
+    string expirationDate
+)
+{ }
+public record PaymentMethod(
+    string id,
+    string type,
+    string activationDate,
+    string expirationDate,
+    string availableBalance,
+    // card number is valued only for credit card type
+    string cardNumber
+)
+{ }
+public record Beneficiary(
+    string id,
+    string fullName,
+    string bankCode,
+    string bankName
+)
+{ }
+
