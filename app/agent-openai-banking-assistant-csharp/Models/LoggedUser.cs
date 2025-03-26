58c42226ac37b783d7be1f671bb28b1989f95b24
David Minkovski <dminkovski@microsoft.com>
2025-03-26 16:23:29 +0100

    Removed java business APIs, added UserService to Agents, Added Payments Business API, Added Transactions Business API

---
+++ app/agent-openai-banking-assistant-csharp/Models/LoggedUser.cs
@@ -0,0 +1,2 @@
+﻿public record LoggedUser(String username, String mail, String role, String displayName)
+{ }
---
+++ app/business-api/payment-api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IPaymentService>(provider =>
+        {
+            var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+            var httpClient = new HttpClient();
+            var transactionsApiUrl = configuration["BackendAPIs:TransactionsApiUrl"];
+            return new PaymentService(loggerFactory, httpClient, transactionsApiUrl);
+        });
+        return services;
+    }
+}
